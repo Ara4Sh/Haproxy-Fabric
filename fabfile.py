6b6f7525dafc58cf66d7d818dcc1ac7ce38591ff
Arash Shams <a.shams@dabacenter.ir>
2015-05-27 11:33:20 +0430

    update , some main functions successfully tested . installation functions not working yet.

--- fabfile.py
+++ fabfile.py
@@ -13,7 +13,10 @@ import keepalivedcfg
 @parallel
 @hosts(setting.LB_hosts)
 def install_full():
-	
+	install_haproxy()
+	install_keepalived()
+	configure_haproxy()
+		
 
 
 @parallel
@@ -25,7 +28,7 @@ def ubuntu_update():
 		run('aptitude update')
 		upgrade = run('apt-get -y upgrade')
 	if upgrade.failed is False:
-	puts('Distribution up to date')
+		puts('Distribution up to date')
 
 @parallel
 @hosts(setting.LB_hosts)
@@ -42,13 +45,55 @@ def install_keepalived():
         if result.failed is False:
                 puts('Keepalived installed successfully')
 @parallel
-@hosts(settings.cluster_hosts)
+@hosts(setting.LB_hosts)
 def configure_haproxy():
 	""" Configuring HAProxy: change parameters in setting.py
 	"""
 	result = run('echo "{haproxycfg}" > /etc/haproxy/haproxy.cfg'.format(
 	haproxycfg=haproxycfg.haproxy_cfg.format(
-		settings=settings)
+		setting=setting)
 		)
 	)
+
+# @parallel
+@hosts(setting.LB_hosts)
+def haproxy(command=None):
+	"""Control the HAProxy : start|stop|reload|restart|status
+	"""
+	if command not in ('start','stop','reload','restart','status'):
+		warn('Unknown command: {}\n'
+			'Available commands: start, stop, restart , reload , status'.format(command))
+	else:
+		if command == 'start':
+			result = run('service haproxy start')
+		elif command == 'stop':
+			result = run('service haproxy stop')
+		elif command == 'reload':
+			result = run('service haproxy reload')
+		elif command == 'restart':
+			result = run('service haproxy restart')
+		elif command == 'status':
+                        result = run('service haproxy status')
+@parallel
+@hosts(setting.LB_hosts)
+def keepalived(command=None):
+        """Control the keepalived : start|stop|restart|reload|force-reload 
+        """
+        if command not in ('start','stop','reload','restart','force-reload'):
+                warn('Unknown command: {}\n'
+                        'Available commands: start, stop, restart , reload , force-reload'.format(command))
+        else:
+                if command == 'start':
+                        result = run('service keepalived start')
+                elif command == 'stop':
+                        result = run('service keepalived stop')
+                elif command == 'reload':
+                        result = run('service keepalived reload')
+                elif command == 'restart':
+                        result = run('service keepalived restart')
+                elif command == 'status':
+                        result = run('service keepalived force-reload')
+                        
+
+
 
